5f808f6681382419d80cacc595b0ac4b50f6b9e4
hungzerommo <129515723+hungzerommo@users.noreply.github.com>
2025-10-20 15:39:36 +0700

    Delete 2010 directory

--- 2010/inspect.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-path = Path("index.html")
-data = path.read_bytes()
-needle = b"secretToggle.textContent = secretToggleDefaultText"
-idx = data.find(needle)
-print(idx)
-print(data[idx:idx+120])
